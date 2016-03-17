ae9bf448fa5edf8d55cf16103777372d13e80f59
Jeff Seymour and Lauryn Davis <lryndavis@gmail.com>
2016-03-17 10:52:30 -0700

    add click handler to each individual bike item that shows div with additional info

--- js/user-interface.js
+++ js/user-interface.js
@@ -1,6 +1,6 @@
 var apiKey = "836d1cb2b10624b566b42f1711ac962df243e9cda4aee63a5d555458f4012530";
 
-titley = "";
+// titley = "";
 
 
 $(function(){
@@ -8,22 +8,26 @@ $(function(){
     event.preventDefault();
     var city = $('#city').val();
     $("#city").val("");
-    $('.bike-info').text("The City you have chosen is " + city + ".");
+    $('.bike-info').prepend("The City you have chosen is " + city + ".");
     $.get('https://bikeindex.org:443/api/v2/bikes_search/stolen?page=1&proximity=' + city + '%2C%20OR&proximity_square=100', function(response) {
         $.each(response.bikes, function(i, bike){
-          console.log(response.bikes);
+
           titley = this.title;
-          $('.bike-info').append('<li class="bike">' + this.title + '</li>');
+          yearModel = this.year;
+          manufacturerName = this.manufacturer_name;
+          stolenFrom = this.stolen_location;
+          stuff = '<div class="bk bike-data' + i + '">' +
+                          '<p>Title: <span class="title">' + titley + '</span></p>' +
+                          '<p>Year: <span class="title">' + yearModel + '</span></p>' +
+                          '<p>Make: <span class="title">' + manufacturerName + '</span></p>' +
+                          '<p>Stolen From: <span class="title">' + stolenFrom + '</span></p>' +
+                        '</div>'
+          $('.bike-info').append('<li class="bike" id=' + i + '>' + this.title + stuff + '</li>');
+          });
+          $('.bike-info').on("click",".bike", function(){
+            $(".bike-data" + this.id).show();
+            console.log(this.id);
+        });
       });
-      $('.bikes .bike-info .bike').click(function(){
-        console.log("hi");
-        $('.bike-stats .title').html(titley);
-      });
-          console.log(response.bikes);
     });
   });
-
-});
-
-
-// Potentially create new bike objects for each when looping thru array of bikey
