116fd010b63eaf606ef7502861f00e21931a3f36
Jeff Seymour and Lauryn Davis <lryndavis@gmail.com>
2016-03-17 14:53:55 -0700

    add error handling and fix a few bugs

---
+++ js/bike.js
@@ -0,0 +1,38 @@
+exports.bikeGetter = function(city) {
+  $.get('https://bikeindex.org:443/api/v2/bikes_search/stolen?page=1&per_page=999&proximity=' + city + '%2C%20OR&proximity_square=100').then(function(response) {
+    $.each(response.bikes, function(i, bike){
+      titley = this.title;
+      yearModel = this.year;
+      manufacturerName = this.manufacturer_name;
+      stolenFrom = this.stolen_location;
+      colors = this.frame_colors;
+      serialNum = this.serial;
+      frameModel = this.frameModel;
+      if (this.thumb !== null) {
+        bikeImage = '<img src="' + this.thumb +'">';
+      } else {
+        bikeImage = "<span>Image is not available</span>";
+      }
+
+      bikeStats = '<div class="bk bike-data' + i + '">' +
+                    '<p><strong>Year: </strong><span class="title">' + yearModel + '</span></p>' +
+                    '<p><strong>Make: </strong><span class="title">' + manufacturerName + '</span></p>' +
+                    '<p><strong>Stolen From: </strong><span class="title">' + stolenFrom + '</span></p>' +
+                    '<p><strong>Colors: </strong><span class="title">' + colors + '</span></p>' +
+                    '<p><strong>Serial Number: </strong><span class="title">' + serialNum + '</span></p>' +
+                    '<p><strong>Frame Model: </strong><span class="title">' + frameModel + '</span></p>' +
+                    '<p>' + bikeImage + '</p>' +
+                  '</div>';
+      $(".searching").show();
+      $(".search-city").text(city);
+      $('.bike-info').append('<li class="bike" id=' + i + '>' + titley + bikeStats + '</li>');
+    });
+      $('.bike-info').off("click",".bike");
+      $('.bike-info').on("click",".bike", function(){
+        $(".bike-data" + this.id).slideToggle();
+    });
+    }).fail(function(error) {
+    $('.bike-info').text(error.responseJSON.message);
+    console.log(error.responseJSON.message);
+  });
+}
--- js/user-interface.js
+++ js/user-interface.js
@@ -1,40 +1,11 @@
-var apiKey = "836d1cb2b10624b566b42f1711ac962df243e9cda4aee63a5d555458f4012530";
-
+var bikeGetter = require('./../js/bike.js').bikeGetter;
 
 $(function(){
   $('#city-form').submit(function(event){
     event.preventDefault();
     var city = $('#city').val();
-    $('.bike-info').text("");
+    $('.bike-info').empty();
     $("#city").val("");
-    $.get('https://bikeindex.org:443/api/v2/bikes_search/stolen?page=1&per_page=999&proximity=' + city + '%2C%20OR&proximity_square=100', function(response) {
-      $.each(response.bikes, function(i, bike){
-        titley = this.title;
-        yearModel = this.year;
-        manufacturerName = this.manufacturer_name;
-        stolenFrom = this.stolen_location;
-        colors = this.frame_colors;
-        if (this.thumb !== "null") {
-          bikeImage = this.thumb;
-        } else {
-          bikeImage = "Image is not available";
-        }
-
-        stuff = '<div class="bk bike-data' + i + '">' +
-                  '<p>Year: <span class="title">' + yearModel + '</span></p>' +
-                  '<p>Make: <span class="title">' + manufacturerName + '</span></p>' +
-                  '<p>Stolen From: <span class="title">' + stolenFrom + '</span></p>' +
-                  '<p>Colors: <span class="title">' + colors + '</span></p>' +
-                  '<p><img src="' + bikeImage +'"></p>' +
-                '</div>'
-        $(".searching").show();
-        $(".search-city").text(city);
-        $('.bike-info').append('<li class="bike" id=' + i + '>' + titley + stuff + '</li>');
-        });
-
-        $('.bike-info').on("click",".bike", function(){
-          $(".bike-data" + this.id).slideToggle();
-      });
-    });
+    bikeGetter(city);
   });
 });
